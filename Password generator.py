f446dcc5fb90b59fa4f257e85620641936af8ffa
Dheeraj Kumar <101472957+ClarineTread@users.noreply.github.com>
2022-03-12 23:21:54 +0530

    Update Password generator.py

--- Password generator.py
+++ Password generator.py
@@ -1,11 +1,11 @@
 from ntpath import join
 import random
-n = int(input("Enter the number of digits for your random password"))
-Upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-Lower_case = "abcdefghijklmnopqrstuvwqyz"
-Num_ = "1234567890"
-Spl_char = "!@#$%^&*.?"
+n = int(input("Enter the number of digits for your random password")) # Max length is 72
+Upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" #Uppercase letters
+Lower_case = "abcdefghijklmnopqrstuvwqyz" #Lowercase letters
+Num_ = "1234567890" #Numbers
+Spl_char = "!@#$%^&*.?" #Special characters
 combined_chars = Upper_case + Lower_case + Num_ + Spl_char
-len = n
+len = n # length based on user input
 psw = "".join(random.sample(combined_chars,len))
-print("Your password is", psw)
+print("Your password is", psw) 
